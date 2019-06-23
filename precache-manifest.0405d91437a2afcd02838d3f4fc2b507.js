b4832fd6cc611d37f552bc88171232a28a9849a8
jk.shin <pooq99@gmail.com>
2019-06-23 13:08:33 +0900

    201906231308

---
+++ precache-manifest.0405d91437a2afcd02838d3f4fc2b507.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a453eee07409f7e602eca87885f09d41",
+    "url": "https://makecherry.com/index.html"
+  },
+  {
+    "revision": "a6a5591d6fd876118a5c",
+    "url": "https://makecherry.com/static/css/2.3c1ddb0a.chunk.css"
+  },
+  {
+    "revision": "433c9d122979dc47bb77",
+    "url": "https://makecherry.com/static/css/main.f48d1717.chunk.css"
+  },
+  {
+    "revision": "a6a5591d6fd876118a5c",
+    "url": "https://makecherry.com/static/js/2.e1936a85.chunk.js"
+  },
+  {
+    "revision": "433c9d122979dc47bb77",
+    "url": "https://makecherry.com/static/js/main.41e3670e.chunk.js"
+  },
+  {
+    "revision": "2bb181ce4290da1c65c4",
+    "url": "https://makecherry.com/static/js/runtime~main.fc51790a.js"
+  }
+]);
